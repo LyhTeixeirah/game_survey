5f64b48e0df2f619236fbb4fc8f93aa267db32f0
LyhTeixeirah <lielses@live.com>
2020-09-14 17:23:56 -0300

    Project create

---
+++ BCK/game_survey/src/test/java/com/lyhteixeirah/game_survey/GameSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyhteixeirah.game_survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameSurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
