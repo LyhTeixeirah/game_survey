b244142ee02e43834b305271d63110e7b0030b6b
LyhTeixeirah <lielses@live.com>
2020-09-17 15:49:28 -0300

    Navigation with Rotes

--- FRN/front-web/src/pages/Home/index.tsx
+++ FRN/front-web/src/pages/Home/index.tsx
@@ -4,15 +4,18 @@ import './styles.css'
 import {ReactComponent as ArrowIcon} from '../../assets/arrow.svg'
 import {ReactComponent as GamerImage} from '../../assets/GamerImage.svg'
 
+import { Link } from 'react-router-dom'
+
 const Home = () => (
     <div className="home-container">
         <div className="home-text">
             <h1 className="home-text-title">
-            Quais <span className="gradiente">Games</span> a galera gosta mais?
+            Quais <span>Games</span> a galera gosta mais?
             </h1>
             <h3 className="home-text-subtitle">
                 Clique no botão abaixo e saiba quais são os jogos que os Gamers estão escolhendo!
             </h3>
+            <Link to="/Records">
             <div className="home-actions">
                 <button className="home-btn">
                     <span> Quero saber quais são</span>
@@ -21,6 +24,7 @@ const Home = () => (
                     <ArrowIcon />
                 </div>
             </div>
+            </Link>
         </div>
         <GamerImage className="home-image" />
 
---
+++ FRN/front-web/src/pages/Records/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.css'
+
+const Records = () => (
+    <div className="home-container">
+        <h1>Records</h1>
+    </div>
+    )
+
+
+export default Records;
