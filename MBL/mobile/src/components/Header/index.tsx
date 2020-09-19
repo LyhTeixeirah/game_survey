43f996cf2164319e2bdbb07888215754edffb57b
LyhTeixeirah <lielses@live.com>
2020-09-19 15:02:33 -0300

    Mobile Project

---
+++ MBL/mobile/src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Header() {
+    return (
+        <View>
+            <Text>Teste 57899</Text>
+        </View>
+    )
+}
+
---
+++ MBL/mobile/src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Home() {
+    return (
+        <View>
+            <Text>Teste 5678899</Text>
+        </View>
+    )
+}
